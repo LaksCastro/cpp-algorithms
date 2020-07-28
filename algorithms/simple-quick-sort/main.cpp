e633347810229e3cbeb1e9cd4028946e124f0a51
Laks Castro <lakhsmycastro336@gmail.com>
2020-07-27 23:10:44 -0300

    Update main.cpp

--- algorithms/simple-quick-sort/main.cpp
+++ algorithms/simple-quick-sort/main.cpp
@@ -4,59 +4,59 @@ using namespace std;
 
 int length;
 
-void quicksort(int values[], int began, int end) {
+void printArray(int array[]) {
+  for (int i = 0; i < length; i++) {
+    cout << "Element " << i << ": ";
+    cout << array[i] << endl;
+  }
+}
+
+void quickSort(int values[], int start, int end) {
   int i, j, pivo, aux;
-  
-  i = began;
+
+  i = start;
   j = end - 1;
-  
-  pivo = values[(began + end) / 2];
-  
+
+  pivo = values[(start + end) / 2];
+
   while (i <= j) {
     while (values[i] < pivo && i < end) {
       i++;
     }
-  
-    while (values[j] > pivo && j > began) {
+
+    while (values[j] > pivo && j > start) {
       j--;
     }
-  
+
     if (i <= j) {
       aux = values[i];
       values[i] = values[j];
       values[j] = aux;
-      
+
       i++;
       j--;
     }
   }
-  
-  if (j > began) quicksort(values, began, j + 1);
-  if (i < end) quicksort(values, i, end);
-}
 
-void printArray(int array[]){
-  for (int i = 0; i < length; i++) {
-    cout << array[i] << ' ';
+  if (j > start) {
+    quickSort(values, start, j + 1);
+  }
+  if (i < end) {
+    quickSort(values, i, end);
   }
 }
 
 int main() {
   int array[] = {5, 8, 1, 2, 7, 3, 6, 9, 4, 10};
-  
+
   length = sizeof(array) / sizeof(array[0]);
-  
-  for (int i = 0; i < length; i++) {
-    cout << array[i] << ' ';
-  }
-  
-  cout << endl;
-  
-  quicksort(array, 0, length);
-  
-  for (int i = 0; i < length; i++) {
-    cout << array[i] << ' ';
-  }
-  
+
+  cout << "Raw Array: " << endl;
+  printArray(array);
+  cout << endl << "Starting Simple Quick Sort Algorithm" << endl;
+  quickSort(array, 0, length);
+  cout << "Done, the sorted array is:" << endl << endl;
+  printArray(array);
+
   return 0;
 }
